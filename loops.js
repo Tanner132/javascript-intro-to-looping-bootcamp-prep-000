ac77dc66ba580268279629451cec4f023917c1f5
Tanner Strickland <tannerstrickland2000@yahoo.com>
2018-08-27 19:43:22 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,28 @@
+function forLoop(array) {
+  for (var i=0; i < 25; i++) {
+    if (i===1){
+      array.push("I am 1 strange loop.")}
+      else {
+         array.push("I am " + i + " strange loops.");
+    }
+ } 
+ return array;
+}
+
+function whileLoop(n) {
+  while(n > 0) {
+    console.log(--n > 0);
+  }return 'done';
+}
+
+function incrementVariable() {
+  i = i + 1;
+}
+
+function doWhileLoop(array) {
+  function incrementVariable() {
+  i = i + 1;
+}
+ do { array.unshift;}
+ while ([array > 0 && incrementVariable()]);
+}
